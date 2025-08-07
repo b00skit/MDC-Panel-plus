c2c92398abd04ebcc28a4b9a358d298f6065c7c1
NU (Booskit) <Redbully14urh@gmail.com>
2025-08-07 18:19:29 +0000

    make the + in MDC Panel a bit more noticable on the sidebar

--- src/components/layout/sidebar-nav.tsx
+++ src/components/layout/sidebar-nav.tsx
@@ -49,7 +49,7 @@ export function SidebarNav() {
           </Link>
            <div className="flex items-center gap-1">
             <span className="text-xl font-semibold font-headline">MDC Panel</span>
-            <span className="font-bold text-primary">+</span>
+            <span className="text-2xl font-bold text-primary drop-shadow-[0_0_3px_hsl(var(--primary)/0.5)]">+</span>
           </div>
         </div>
       </SidebarHeader>
