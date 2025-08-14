27428fe0f21b16605da3d0c0ec4ef66e45c7414a
NU (Booskit) <Redbully14urh@gmail.com>
2025-08-14 09:13:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,39 +1,17 @@
+
 'use client';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import dynamic from 'next/dynamic';
+import { useMemo } from 'react';
+import { Skeleton } from '@/components/ui/skeleton';
 
-// Fix for default icon issue with Webpack
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-  iconUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  shadowUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+const MapComponent = dynamic(() => import('./map-component'), {
+  ssr: false,
+  loading: () => <Skeleton className="h-full w-full rounded-lg" />,
 });
 
 const Map = () => {
-  return (
-    <MapContainer
-      center={[-45, -20]}
-      zoom={4}
-      maxZoom={5}
-      minZoom={2}
-      scrollWheelZoom={true}
-      style={{ height: '100%', width: '100%', borderRadius: '0.5rem' }}
-      className="z-0"
-    >
-      <TileLayer
-        url="/img/mapStyles/styleStreet/{z}/{x}/{y}.jpg"
-        attribution='San Andreas Street Guide - MDC'
-        noWrap={true}
-      />
-    </MapContainer>
-  );
+  return <MapComponent />;
 };
 
 export default Map;
