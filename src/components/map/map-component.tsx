126b36f46181eb51cb36aa9ee0b95d7aef2af0a0
NU (Booskit) <Redbully14urh@gmail.com>
2025-08-14 11:16:32 +0000

    3.1.0 - interactive map
    remove the search street button, we're just gon

--- src/components/map/map-component.tsx
+++ src/components/map/map-component.tsx
@@ -52,15 +52,15 @@ const SearchComponent = ({ streets }: { streets: any[] }) => {
                 },
                 initial: false,
                 collapsed: false,
-                textPlaceholder: 'Search Street...'
+                textPlaceholder: 'Search Street...',
+                textCancel: ''
             });
 
             map.addControl(searchControl);
             
-            // Add search icon to the button
             const searchButton = document.querySelector('.leaflet-control-search .search-button');
-            if (searchButton) {
-                searchButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>';
+            if (searchButton && searchButton.parentNode) {
+                searchButton.parentNode.removeChild(searchButton);
             }
 
 
