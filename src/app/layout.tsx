9cb06a311ca9bc350b20ba4cc1ed2f485ebda5ed
NU (Booskit) <Redbully14urh@gmail.com>
2025-08-19 12:59:15 +0000

    3.1.1 - analytics
    make a small modification to the analytics script, it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,21 +94,23 @@ export default async function RootLayout({
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.css"
         />
-        <Script id="matomo-tracking" strategy="afterInteractive">
-          {`
-            var _paq = window._paq = window._paq || [];
-            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-              var u="//sys.booskit.dev/analytics/";
-              _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '1']);
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-            })();
-          `}
-        </Script>
+        {process.env.NODE_ENV === 'production' && (
+          <Script id="matomo-tracking" strategy="afterInteractive">
+            {`
+              var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="//sys.booskit.dev/analytics/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+            `}
+          </Script>
+        )}
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
