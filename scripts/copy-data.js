66e5a3aefa8dab3118c171fffc595756dc39beeb
Biscuit <gtaw@booskit.dev>
2025-08-29 16:59:36 +0200

    chore: copy data in dev and build scripts

---
+++ scripts/copy-data.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const srcDir = path.resolve(__dirname, '..', 'data');
+const destDir = path.resolve(__dirname, '..', 'public', 'data');
+
+fs.rmSync(destDir, { recursive: true, force: true });
+fs.cpSync(srcDir, destDir, { recursive: true });
+
+console.log(`Copied data from ${srcDir} to ${destDir}`);
+
