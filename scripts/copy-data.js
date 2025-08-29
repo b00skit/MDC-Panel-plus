21366407193e6a3321ec6cbbeba29ad057fe95b5
NU (Booskit) <Redbully14urh@gmail.com>
2025-08-29 15:06:41 +0000

    copy-data script minor log adjustment

--- scripts/copy-data.js
+++ scripts/copy-data.js
@@ -4,6 +4,8 @@ const path = require('path');
 const srcDir = path.resolve(__dirname, '..', 'data');
 const destDir = path.resolve(__dirname, '..', 'public', 'data');
 
+console.log(`Copying data from ${srcDir} to ${destDir}`);
+
 fs.rmSync(destDir, { recursive: true, force: true });
 fs.cpSync(srcDir, destDir, { recursive: true });
 
