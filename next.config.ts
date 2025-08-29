9556a27b848f44aa1130179d94af82732ff66130
Biscuit <gtaw@booskit.dev>
2025-08-29 09:59:25 +0200

    Move beta config to data config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type {NextConfig} from 'next';
+import config from './data/config.json';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -9,8 +10,8 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   env: {
-    NEXT_PUBLIC_BETA_ENABLED: process.env.BETA_ENABLED,
-    NEXT_PUBLIC_BETA_CODE: process.env.BETA_CODE,
+    NEXT_PUBLIC_BETA_ENABLED: config.BETA_ENABLED,
+    NEXT_PUBLIC_BETA_CODE: config.BETA_CODE,
   }
 };
 
